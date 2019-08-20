09530e6d96c3dae36f7578bb24b7c2f6a1d4150f
utdawn <342927963@qq.com>
2019-08-20 10:31:15 +0800

    first commit

---
+++ src/main/java/com/gcwl/bkserver/BkServerApplication.java
@@ -0,0 +1,13 @@
+package com.gcwl.bkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkServerApplication.class, args);
+    }
+
+}
